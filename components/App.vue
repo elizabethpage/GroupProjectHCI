947fb51ea6e3ad0d632b332abf0d717674f637a9
ellamiller30 <124221919+ellamiller30@users.noreply.github.com>
2024-04-26 09:11:31 -0500

    attempt at csv

--- components/App.vue
+++ components/App.vue
@@ -6,7 +6,7 @@
     import { ref, onMounted, computed } from 'vue';
 
     // data imported
-    const MovieData = ref([]);
+    const movieData = ref([]);
 
     // title
     const title = ref([]);
@@ -17,8 +17,12 @@
     // for filtering based on text
     const filterText = ref('');
 
+    const sortDirection = ref(1);
+    const whatToSort = ref('Series_Title');
+
     // location of data
     const dataFile = "./assets/imdb_top_1000.csv";
+    
 
     // asynchronously loads data when page is mounted
     onMounted(() => fetch(dataFile)
@@ -29,7 +33,7 @@
     // asynchronous function to wrap around parse process
     async function loadData(dataText) {
         console.log("ready to load data");
-        let data = papaparse.parse(dataText, {delimter: ",", header: true}) // NOT DONE
+        let data = papaparse.parse(dataText, {delimter: ",", header: true});
         return data;
     }
 
@@ -39,36 +43,34 @@
     
 
     // get data 
-    MovieData.value = parsedData.data;
+    movieData.value = parsedData.slice(2);
 
     // show data
     loaded.value = true;
-
     }
+
+    // dynamically generated filtered and sorted data
+    const filteredMovieData = computed(
+        () => {
+            // first, filter by title
+            let filteredByTitle = movieData.value;
+
+            filteredByTitle.sort((a, b) => {
+                var start = a[whatToSort.value];
+                var end = b[whatToSort.value];
+
+                if (whatToSort.value == 1) {
+                    start = Number(a[whatToSort.value]);
+                    end = Number(b[whatToSort.value]);
+                }
+            });
+        }
+    )
 
 
 </script>
 
-<template>
-    <div id="app">
-      <HtmlViewer :htmlPaths="['/HomePage.html', '/Movies.html']" />
-    </div>
-  </template>
-  
-  <script>
-  import HtmlViewer from './components/HtmlViewer.vue';
-  
-  export default {
-    name: 'App',
-    components: {
-      HtmlViewer
-    }
-  }
-  </script>
-  
-  <style>
-  /* Add global styles if needed */
-  </style>
+
   
 
 
@@ -77,6 +79,25 @@
         
         <div v-if="loaded">
         
+            <!-- Title-->
+            <div class="row">
+                <h1>Movies</h1>
+                <hr/>
+            </div>
+
+            <!-- table body with data -->
+            <tbody>
+                <tr v-for="movie in filteredMovieData">
+                    <td>{{ movie.Series_Title }}</td>
+                    <td>{{ movie.Released_Year }} </td>
+                </tr>
+            </tbody>
+
+
+        </div>
+
+        <div v-else>
+            <p>Loading data...</p>
         </div>
     </div>
 </template>
